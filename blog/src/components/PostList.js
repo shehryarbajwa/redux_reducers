fcf7f32fce67c2240e018b30b428aa9cd116f5e1
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-06-04 00:38:48 +0500

    commented out index.js and postlist

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
+//When component gets Mounted, make an action to make an API call
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default connect(
-  null,
-  { fetchPosts }
-)(PostList);
+//Adding state to props for this component
+//By declaring posts: state.posts
+//We have declared the posts variable and that variable will now lead to be used within the component
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+//Here in the connect function, we pass mapStateToProps, then an action and the component so when those
+//changes are made in the action, the component listens.
+export default connect(mapStateToProps, { fetchPosts })(PostList);
