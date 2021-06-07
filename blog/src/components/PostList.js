21bb0ab507bb80a784216006edbd6cea7ab77a4a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-06-07 15:22:19 +0500

    added blogs

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,19 +8,37 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props.posts.data);
-    return <div>Post List</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
 //Adding state to props for this component
 //By declaring posts: state.posts
 //We have declared the posts variable and that variable will now lead to be used within the component
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return { posts: state.posts };
 };
 
 //Here in the connect function, we pass mapStateToProps, then an action and the component so when those
 //changes are made in the action, the component listens.
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostList);
