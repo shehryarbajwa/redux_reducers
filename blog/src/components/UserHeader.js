4f4bf78946ca7793be6095e9acd0a1c493238a54
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-06-09 03:59:10 +0500

    added ownProps to mapStateToProps

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
+  //This.props can use the action creator we have and pass it the props value we have defined
   componentDidMount() {
     this.props.fetchUser(this.props.userId);
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props.user
 
     if (!user) {
       return null;
@@ -17,9 +18,9 @@ class UserHeader extends React.Component {
     return <div className="header">{user.name}</div>;
   }
 }
-
-const mapStateToProps = (state) => {
-  return { users: state.users };
+//Own Props is a copy of the props we are about to send in the component
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
